f6b3ea7a0f261ec4150e3c4a082e569643732a15
fairyaksh <akshayasatheesh@hotmail.co.uk>
2022-12-20 16:43:51 +0000

    Feature: Creating function to send get request to Google Books API using user's input and api key as params

--- bin/index.js
+++ bin/index.js
@@ -5,3 +5,7 @@ process.removeAllListeners("warning");
 import inquirer from "inquirer";
 import clear from 'clear';
 dotenv.config()
+
+const searchBookFromUserInput = (userInput) => {
+    const getUrl = `https://www.googleapis.com/books/v1/volumes?q=${userInput}&key=${process.env.API_KEY}`;
+}
