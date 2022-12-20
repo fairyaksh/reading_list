fb92635310e292c9d475d80326503e4284567991
fairyaksh <akshayasatheesh@hotmail.co.uk>
2022-12-20 16:35:37 +0000

    Update: Removing CLI warning to clear clutter

--- bin/index.js
+++ bin/index.js
@@ -1,5 +1,7 @@
 #! /usr/bin/env node
 
+// removes warning: (https://github.com/netlify/cli/issues/4608#issuecomment-1223696635)
+process.removeAllListeners("warning"); 
 import inquirer from "inquirer";
 import clear from 'clear';
 dotenv.config()
